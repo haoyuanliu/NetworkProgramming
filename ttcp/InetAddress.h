9b9afae42b09e6872be3c83e98e5c31fe898b78c
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-13 15:46:43 +0800

    add InetAddress h file

--- ttcp/InetAddress.h
+++ ttcp/InetAddress.h
@@ -10,15 +10,28 @@ class InetAddress : copyable
         InetAddress(StringArg ip, uint16_t port);
         explicit InetAddress(StringArg ipPort);
         explicit InetAddress(unit16_t port, bool loopbackOnly = false);
-        explicit InetAddress(const struct sockaddr_in& saddr) : saddr_(addr) { }
+        explicit InetAddress(const struct sockaddr_in& saddr) : saddr_(saddr) { }
 
         std::string toIp() const;
         std::string toIpPort() const;
         const struct sockaddr_in& getSockAddrInet() const { return saddr_; }
         void setSockAddrInet(const struct sockaddr_in& saddr) { saddr_ = saddr; }
-        void setPort(uint16_t port) { saddr_.sin_p = htons(port); }
+        void setPort(uint16_t port) { saddr_.sin_port = htons(port); }
+        uint32_t ipNetEndian() const { return saddr_.sin_addr.s_addr; }
+        uint16_t portNetEndian() const { return saddr_.sin_port; }
+        uint32_t ipHostEndian() const { return ntohl(saddr_.sin_addr.s_addr); }
+        uint16_t portHostEndian() const { return ntohs(saddr_.sin_port); }
 
+        static bool resolve(StringArg hostname, InetAddress*);
+        static std::vector<InetAddress> resolveAll(StringArg hostname, uint16_t port = 0);
+
+        bool operator==(const InetAddress& rhs) const
+        {
+            return saddr_.sin_family == rhs.saddr_.sin_family
+                && ipNetEndian() == rhs.ipNetEndian()
+                && portNetEndian() == rhs.portNetEndian();
+        }
     private:
         static bool resolveSlow(const char *hostname, InetAddress*);
-        struct sockaddr_in addr_;
+        struct sockaddr_in saddr_;
 }
