a3678f146ae5e3f758b2e394085cfcaeacf7fdd1
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-14 14:37:08 +0800

    add InetAddress files

--- ttcp/InetAddress.cpp
+++ ttcp/InetAddress.cpp
@@ -29,3 +29,64 @@ std::string InetAddress::toIpPort() const
     snprintf(buf + end, sizeof buf - end, ":%u", port);
     return buf;
 }
+
+static const int kResolveBufSize = 4096;
+
+bool InetAddress::resolveSlow(const char* hostname, InetAddress* out)
+{
+    std::vector<char> buf(2 * kResolveBufSize);
+    struct hostent hent;
+    struct hostent* he = NULL;
+    int herrno = 0;
+    bzero(&hent, sizeof(hent));
+
+    while(buf.size() <= 16 * kResolveBufSize)
+    {
+        int ret = gethostbyname_r(hostname, &hent, buf.data(), buf.size(), &he, &herrno);
+        if(ret == 0 && he != NULL)
+        {
+            assert(he->h_addrtype == AF_INET && he->h_length == sizeof(uint32_t));
+            out->saddr_.sin_addr = *reinterpret_cast<struct in_addr*>(he->h_addr);
+            return true;
+        }
+        else if(ret == ERANGE)
+        {
+            buf.resize(buf.size() * 2);
+        }
+        else
+        {
+            if(ret)
+                perror("InetAddress::resolve");
+            return false;
+        }
+    }
+    return false;
+}
+
+bool InetAddress::resolve(StringArg hostname, InetAddress* out)
+{
+    arrset(out);
+    char buf[kResolveBufSize];
+    struct hostent hent;
+    struct hostent* he = NULL;
+    int herrno = 0;
+    bzero(&hent, sizeof(hent));
+
+    int ret = gethostbyname_r(hostname.c_str(), &hent, buf, sizeof buf, &he, &herrno);
+    if(ret == 0 && he != NULL)
+    {
+        assert(he->h_addrtype == AF_INET && he->h_length == sizeof(uint32_t));
+        out->saddr_.sin_addr = *reinterpret_cast<struct in_addr*>(he->h_addr);
+        return true;
+    }
+    else if(ret == ERANGE)
+    {
+        return resolveSlow(hostname.c_str(), out);
+    }
+    else
+    {
+        if(ret)
+            perror("InetAddress::resolve");
+        return false;
+    }
+}
