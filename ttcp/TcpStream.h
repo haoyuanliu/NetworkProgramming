cf0c0c696272aeb6581ba80f7540663e3f4b46eb
zuilehongdou <15120101@bjtu.edu.cn>
2017-01-19 15:38:26 +0800

    add TcpStream.h file

--- ttcp/TcpStream.h
+++ ttcp/TcpStream.h
@@ -10,7 +10,21 @@ typedef std::unique_ptr<TcpStream> TcpStreamPtr;
 class TcpStream : noncopyable
 {
     public:
+        explicit TcpStream(Socket&& sock);
+        ~TcpStream() = default;
+        TcpStream(TcpStream&&) = default;
+        TcpStream& operator=(TcpStream&&) = default;
+        static TcpStreamPtr connect(const InetAddress& serverAddr);
+        static TcpStreamPtr connect(const InetAddress& serverAddr, const InetAddress& localAddr);
 
+        int receiveAll(void *buf, int len);
+        int reveiveSome(void *buf, int len);
+
+        int sendAll(const void* buf, int len);
+        int sendSome(const void *buf, int len);
+
+        void setTcpNoDelay(bool on);
+        void shutdownWrite();
     private:
         static TcpStreamPtr connectInternal(const InetAddress& serverAddr, const InetAddress* localAddr);
         Socket sock_;
