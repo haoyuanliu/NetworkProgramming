43a1593abdfa66d2c23c1725c07b9d4c6ae18aef
haoyuanliu <15120101@bjtu.edu.cn>
2017-02-09 17:02:54 +0800

    update sh file

--- ttcp/build.sh
+++ ttcp/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SRCS = "Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp"
+SRCS="Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp"
 
 set -x
 CC=${CC:-g++}
 
-$CC -std=c++11 -Wall -Wextra -g $SRCS ttcp.cpp -o ttcp -lboost_program_options
+$CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp ttcp.cpp -o ttcp -lboost_program_options
