508df302b5b75a8cf3f5dfe4d2c6b398331bc19a
haoyuanliu <15120101@bjtu.edu.cn>
2017-02-11 16:51:21 +0800

    add echo files

--- tpc/build.sh
+++ tpc/build.sh
@@ -7,3 +7,4 @@ CC=${CC:-g++}
 
 $CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp ttcp.cpp -o ttcp -lboost_program_options
 $CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp echoClient.cpp -o echoClient -lboost_program_options
+$CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp echo.cpp -o echo -lboost_program_options
---
+++ tpc/echo.cpp
@@ -0,0 +1,37 @@
+#include "Acceptor.h"
+#include "InetAddress.h"
+#include "TcpStream.h"
+
+#include <thread>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    InetAddress listenAddr(3007);
+    Acceptor acceptor(listenAddr);
+    printf("Accepting... Ctrl-C to exit\n");
+    int count = 0;
+    bool nodelay = argc > 1 && strcmp(argv[1], "-D") == 0;
+    while(true)
+    {
+        TcpStreamPtr tcpStream = acceptor.accept();
+        printf("Accepted No.%d client\n", ++count);
+        if(nodelay)
+            tcpStream->setTcpNoDelay(true);
+
+        std::thread thr([count] (TcpStreamPtr stream)
+        {
+            printf("Thread for No.%d client started.\n", count);
+            char buf[4096];
+            int nr = 0;
+            while((nr = stream->receiveSome(buf, sizeof buf)) > 0)
+            {
+                int nw = stream->sendAll(buf, nr);
+                if(nw < nr)
+                    break;
+            }
+            printf("Thread for No.%d client ended.\n", count);
+        }, std::move(tcpStream));
+        thr.detach();
+    }
+}
