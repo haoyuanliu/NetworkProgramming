6a8f6880ba04c7124003aa0a5dab5a39b07c7a38
haoyuanliu <15120101@bjtu.edu.cn>
2017-02-14 15:06:28 +0800

    update build file

--- tpc/build.sh
+++ tpc/build.sh
@@ -5,6 +5,7 @@ SRCS="Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp"
 set -x
 CC=${CC:-g++}
 
-$CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp ttcp.cpp -o ttcp -lboost_program_options
-$CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp echoClient.cpp -o echoClient
-$CC -std=c++11 -Wall -Wextra -g Acceptor.cpp InetAddress.cpp TcpStream.cpp Socket.cpp echo.cpp -o echo -lpthread
+$CC -std=c++11 -Wall -Wextra -g $SRCS ttcp.cpp -o ttcp -lboost_program_options
+$CC -std=c++11 -Wall -Wextra -g $SRCS echoClient.cpp -o echoClient
+$CC -std=c++11 -Wall -Wextra -g $SRCS echo.cpp -o echo -lpthread
+$CC -std=c++11 -Wall -Wextra -g $SRCS roundTripUDP.cpp -o roundTripUDP -lpthread
