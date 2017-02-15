c55afc9c236061e745b43b4bf37ad87cd4d81e0c
haoyuanliu <15120101@bjtu.edu.cn>
2017-02-15 17:21:05 +0800

    update shell build file

--- tpc/build.sh
+++ tpc/build.sh
@@ -9,3 +9,5 @@ $CC -std=c++11 -Wall -Wextra -g $SRCS ttcp.cpp -o ttcp -lboost_program_options
 $CC -std=c++11 -Wall -Wextra -g $SRCS echoClient.cpp -o echoClient
 $CC -std=c++11 -Wall -Wextra -g $SRCS echo.cpp -o echo -lpthread
 $CC -std=c++11 -Wall -Wextra -g $SRCS roundTripUDP.cpp -o roundTripUDP -lpthread
+$CC -std=c++11 -Wall -Wextra -g $SRCS nodelayClient.cpp -o nodelayClient
+$CC -std=c++11 -Wall -Wextra -g $SRCS nodelayServer.cpp -o nodelayServer
